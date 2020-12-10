f644d9c09b9dd33aa89f41a3fd136d9d1b0321eb
kei-yokoyama <74888936+kei-yokoyama@users.noreply.github.com>
2020-12-10 14:56:30 +0900

    コントローラー記述内容表示（PR用）

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,6 @@
 class CalendarsController < ApplicationController
 
+
   # １週間のカレンダーと予定が表示されるページ
   def index
     get_week
@@ -35,6 +36,7 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
+
       wday_num = Date.today.wday + x
       if wday_num >= 7
         wday_num = wday_num -7
@@ -42,7 +44,9 @@ class CalendarsController < ApplicationController
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday:wdays[wday_num]}
 
       @week_days.push(days)
+
     end
 
   end
 end
+
