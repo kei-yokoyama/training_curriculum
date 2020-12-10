b2289baff197bf3d16e0dc48f23925a5964193d4
kei-yokoyama <74888936+kei-yokoyama@users.noreply.github.com>
2020-12-10 15:01:00 +0900

    Revert "コントローラー記述内容表示（PR用）"
    This reverts commit ec7c190ba2eda125893a1def8bddd45cd52095a7, reversing
    changes made to f644d9c09b9dd33aa89f41a3fd136d9d1b0321eb.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -16,7 +16,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
@@ -37,13 +37,11 @@ class CalendarsController < ApplicationController
       end
 
 
-
       wday_num = Date.today.wday + x
       if wday_num >= 7
         wday_num = wday_num -7
       end
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday:wdays[wday_num]}
-
 
       @week_days.push(days)
 
